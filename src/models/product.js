88b77f8290e0e4e2bd2e4e45418dc4c4c623f1fd
dungPtitt <lvdfullstack@gmail.com>
2023-03-13 22:32:49 +0700

    create model and migration

---
+++ src/models/product.js
@@ -0,0 +1,30 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Product extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Product.init({
+    idGroup: DataTypes.INTEGER,
+    nameP: DataTypes.STRING,
+    priceP: DataTypes.INTEGER,
+    countP: DataTypes.INTEGER,
+    imageP: DataTypes.BLOB,
+    infoP: DataTypes.STRING,
+    parameterP: DataTypes.STRING
+    
+  }, {
+    sequelize,
+    modelName: 'Product',
+  });
+  return Product;
+};
